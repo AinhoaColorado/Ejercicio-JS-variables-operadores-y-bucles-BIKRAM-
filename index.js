8957da09375c91f1803367f81ac95bf3e0c8e14b
Ainhoa Colorado <ainhoa.colorado.art@gmail.com>
2025-03-22 13:43:52 +0100

    comienzo

---
+++ index.js
@@ -0,0 +1,8 @@
+let variableSinValor;
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = PI*2;
+
+let booleanoAnd = booleano1 === booleano2;
+let booleanoNot = !booleano1;
